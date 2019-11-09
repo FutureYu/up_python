20a042710f98c0dfa643c4c5c03004d3c6091be8
ubuntu <ubuntu@localhost.localdomain>
2019-11-09 09:20:36 +0800

    增加周数设置

--- tools.py
+++ tools.py
@@ -2,11 +2,13 @@ import time
 class TimeTool:
     @staticmethod
     def Stamp2FullStr(stamp):
+        startstamp = 1567353600
+        weekNum = (stamp - startstamp) // 604800 + 1
         week = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
         timeArray = time.localtime(stamp)
         endTime1 = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
         endTime2 = time.strftime("%w", timeArray)
-        return f"{endTime1} {week[int(endTime2)]}"
+        return f"{endTime1} 第{weekNum}周 {week[int(endTime2)]}"
 
     @staticmethod
     def Stamp2Str(stamp):
